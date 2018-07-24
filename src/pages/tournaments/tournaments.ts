9bb12870b0716f3c0616a51b1437aa4e75a6a18d
Ivan Kapelyukh <ivankapelyukh@gmail.com>
2018-07-24 22:25:03 +0100

    Removed boilerplate comments and fixed file ending

--- src/pages/tournaments/tournaments.ts
+++ src/pages/tournaments/tournaments.ts
@@ -2,13 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
 
-/**
- * Generated class for the TournamentsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-tournaments',
