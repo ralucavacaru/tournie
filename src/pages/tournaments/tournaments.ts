d99a294d5ba6a5cfe03a60d7f1763666e8f9e6f0
ivan-kapelyukh <ivan.kapelyukh@gmail.com>
2018-07-26 00:24:16 +0100

    Added async tournaments GET call on WillEnter

--- src/pages/tournaments/tournaments.ts
+++ src/pages/tournaments/tournaments.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { TournamentDetailPage } from '../tournament-detail/tournament-detail';
+import { Http } from '@angular/http';
+
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 
 @IonicPage()
 @Component({
@@ -10,14 +15,21 @@ import { TournamentDetailPage } from '../tournament-detail/tournament-detail';
 export class TournamentsPage {
 
   tournaments: any = [];
+  loadTournamentsURL = "http://www.eudc-app.com/debate/tournament/get-all";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http) {
     this.tournaments = [
       {expanded: false, name: "Belgrade Open"},
       {expanded: false, name: "Novi Sad EUDC"},
       {expanded: false, name: "Lorem Ipsum IV"},
       {expanded: false, name: "Amet Open"},
     ];
+  }
+
+  ionViewWillEnter() {
+    console.log("ionViewWillEnter TournamentsPage");
+    this.tournaments = this.loadTournaments();
+    console.log(this.tournaments);
   }
 
   ionViewDidLoad() {
@@ -28,6 +40,11 @@ export class TournamentsPage {
     this.navCtrl.push(TournamentDetailPage, {
       tournament: tournament,
     });
+  }
+
+  async loadTournaments() {
+    return (await this.http.get(this.loadTournamentsURL))
+                           .map(res => res.json().name);
   }
 
 }
