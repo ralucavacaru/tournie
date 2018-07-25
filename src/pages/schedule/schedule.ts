b3e4eafd1fa0c6c6cf267c09c3fcab3a38036883
ralucavacaru <raluca_vacaru@yahoo.com>
2018-07-25 04:30:19 +0100

    Removed some commented code & fixed linter errors

--- src/pages/schedule/schedule.ts
+++ src/pages/schedule/schedule.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { EventDetailComponent } from '../../components/event-detail/event-detail';
+import { IonicPage, NavController } from 'ionic-angular';
 
-/**
- * Generated class for the SchedulePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
--- src/pages/tournaments/tournaments.ts
+++ src/pages/tournaments/tournaments.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { HomePage } from '../home/home';
 import { TournamentDetailPage } from '../tournament-detail/tournament-detail';
 
 @IonicPage()
