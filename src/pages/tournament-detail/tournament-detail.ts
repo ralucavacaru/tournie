1419732a0ec9effc61f17ce6816c37eaee21e476
ralucavacaru <raluca_vacaru@yahoo.com>
2018-07-29 19:05:51 +0200

    Made some UI changes

--- src/pages/tournament-detail/tournament-detail.ts
+++ src/pages/tournament-detail/tournament-detail.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Storage } from '@ionic/storage';
 import { RestProvider } from '../../providers/rest/rest';
 import { ConfirmNamePage } from '../confirm-name/confirm-name';
 
@@ -18,7 +17,6 @@ export class TournamentDetailPage {
   constructor(
               public navCtrl: NavController, 
               public navParams: NavParams, 
-              private storage: Storage, 
               public restProvider: RestProvider) {
   	this.tournament = this.navParams.get('tournament');
   }
